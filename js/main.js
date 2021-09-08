1f302f7ac737622552c741a462679dc974baa86c
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-09-08 15:24:06 +0200

    Slider photo with click

--- js/main.js
+++ js/main.js
@@ -9,12 +9,29 @@ const app = new Vue(
     {
         el: "#root",
         data: {
+            counterPhotos: 0,
             photos: [
                 'img/image1.jpg',
                 'img/image2.jpg',
                 'img/image3.jpg',
                 'img/image4.jpg'
             ]
+        },
+        methods: {
+            previousPhoto() {
+                this.counterPhotos -= 1;
+                if (this.counterPhotos < 0) {
+                    this.counterPhotos = (this.photos.length - 1);
+                }
+            },
+            nextPhoto() {
+                this.counterPhotos += 1;
+                if (this.counterPhotos == (this.photos.length)) {
+                    this.counterPhotos = 0;
+                }
+            }
+
+            
         }
     }
 )
